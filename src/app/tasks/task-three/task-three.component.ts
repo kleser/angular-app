6ef70e5dc7f2fd169fcfc4f1da5b0916fe05bebc
Lias Kleisa <lias.kleisa@gmx.ch>
2025-02-23 15:19:32 +0100

    Work

--- src/app/tasks/task-three/task-three.component.ts
+++ src/app/tasks/task-three/task-three.component.ts
@@ -1,48 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-task-three',
-  templateUrl: './task-three.component.html',
-  styleUrls: ['./task-three.component.css']
+    selector: 'app-task-three',
+    templateUrl: './task-three.component.html',
+    styleUrls: ['./task-three.component.css']
 })
 export class TaskThreeComponent implements OnInit {
 
-  isChecked = true;
-  paceOne: string = '5:23';
-  paceTwo: string = '5:28';
-  paceCorrect: boolean = false;
-  kmOne: number = 6.83;
-  kmTwo: number = 6.52;
-  kmCorrect: boolean = false;
+    isChecked = true;
+    paceOne: string = '5:23';
+    paceTwo: string = '5:28';
+    paceCorrect: boolean = false;
+    kmOne: number = 6.83;
+    kmTwo: number = 6.52;
+    kmCorrect: boolean = false;
 
-  falseTry: boolean = false;
+    falseTry: boolean = false;
 
-  constructor(private router: Router) { }
+    firstDone: boolean = false;
 
-  ngOnInit(): void {
-  }
-
-  checkPace(event: any) {
-    let input: string = event.target.value;
-    if (input == this.paceOne || input == this.paceTwo) {
-      this.paceCorrect = true;
+    constructor(private router: Router) {
     }
-  }
 
-  checkKm(event: any) {
-    let input: number = event.target.value;
-    if (input == this.kmOne || input == this.kmTwo) {
-      this.kmCorrect = true;
+    ngOnInit(): void {
     }
-  }
 
-  control() {
-    if (this.kmCorrect && this.paceCorrect && !this.isChecked) {
-      this.router.navigate(['tasks/four']);
-    } else {
-      this.falseTry = true;
+    checkPace(event: any) {
+        let input: string = event.target.value;
+        if (input == this.paceOne || input == this.paceTwo) {
+            this.paceCorrect = true;
+        }
     }
-  }
+
+    checkKm(event: any) {
+        let input: number = event.target.value;
+        if (input == this.kmOne || input == this.kmTwo) {
+            this.kmCorrect = true;
+        }
+    }
+
+    control() {
+        if (this.kmCorrect && this.paceCorrect && !this.isChecked) {
+            this.firstDone = true;
+        } else {
+            this.falseTry = true;
+        }
+    }
+
+    secondControl(event: any) {
+        let input: number = event.target.value;
+        if (input == 13) {
+            this.router.navigate(['tasks/four']);
+        }
+    }
 
 }
